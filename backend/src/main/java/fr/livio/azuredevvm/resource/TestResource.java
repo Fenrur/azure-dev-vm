c9fb512dcb05fb0490ee9d5d5667a4df0b6af8ed
Livio Tinnirello <tinnirellolivio@gmail.com>
2024-02-27 19:59:33 +0100

    chore(backend): Changement pour TestResource

--- backend/src/main/java/fr/livio/azuredevvm/resource/TestResource.java
+++ backend/src/main/java/fr/livio/azuredevvm/resource/TestResource.java
@@ -9,11 +9,13 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.Cache;
+import org.jboss.resteasy.reactive.NoCache;
 import org.jboss.resteasy.reactive.ResponseStatus;
 
-import java.util.HashMap;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.List;
-import java.util.UUID;
 
 @Path("/test")
 @RunOnVirtualThread
@@ -25,14 +27,45 @@ public class TestResource {
     @Inject
     VirtualMachineService virtualMachineService;
 
-    public record TestResponseBody(HashMap<String, Long> map) {
+    public record TestResponseBody(String firstName, String lastName) {
 
     }
+
+    public static final List<String> FIRST_NAMES = List.of(
+            "Emma",
+            "Lucas",
+            "Chloé",
+            "Hugo",
+            "Alice",
+            "Léo",
+            "Lina",
+            "Gabriel",
+            "Zoé",
+            "Raphaël"
+    );
+
+    public static final List<String> LAST_NAMES = List.of(
+            "Martin",
+            "Bernard",
+            "Dubois",
+            "Thomas",
+            "Robert",
+            "Richard",
+            "Petit",
+            "Durand",
+            "Leroy",
+            "Moreau"
+    );
+
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @ResponseStatus(200)
-    public List<UUID> test() throws InterruptedException {
-        return null;
+    public TestResponseBody test() throws InterruptedException, NoSuchAlgorithmException {
+        SecureRandom random = SecureRandom
+                .getInstanceStrong();
+        String firstName = FIRST_NAMES.get(random.nextInt(FIRST_NAMES.size()));
+        String lastName = LAST_NAMES.get(random.nextInt(LAST_NAMES.size()));
+        return new TestResponseBody(firstName, lastName);
     }
 }

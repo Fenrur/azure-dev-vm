7e937b1adbd44f4a4742dc0025bb93c2464153f8
Livio Tinnirello <tinnirellolivio@gmail.com>
2024-03-04 11:35:35 +0100

    feat(backend): Suppression des tests

--- backend/src/test/java/fr/livio/azuredevvm/JpaSecurityReactiveRealmTestIT.java
+++
@@ -1,7 +0,0 @@
-package fr.livio.azuredevvm;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class JpaSecurityReactiveRealmTestIT extends JpaSecurityReactiveRealmTest {
-}

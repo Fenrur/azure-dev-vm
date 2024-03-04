955cd74a5a209b867c608342f3fcfda1731d0bd5
Livio Tinnirello <tinnirellolivio@gmail.com>
2024-03-04 12:11:10 +0100

    feat(backend): Renommage des variables

--- backend/src/main/java/fr/livio/azuredevvm/MaxThresholdVirtualMachine.java
+++ backend/src/main/java/fr/livio/azuredevvm/MaxThresholdVirtualMachine.java
@@ -6,16 +6,16 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 @Singleton
 public class MaxThresholdVirtualMachine {
 
-    @ConfigProperty(name = "azure.global.max-vms", defaultValue = "0")
+    @ConfigProperty(name = "max-vms.global", defaultValue = "0")
     int maxGlobalVirtualMachines;
 
-    @ConfigProperty(name = "azure.global.max-vms.role.admin", defaultValue = "0")
+    @ConfigProperty(name = "max-vms.role.admin", defaultValue = "0")
     int maxVirtualMachinesWithAdminRole;
 
-    @ConfigProperty(name = "azure.global.max-vms.role.advanced", defaultValue = "0")
+    @ConfigProperty(name = "max-vms.role.advanced", defaultValue = "0")
     int maxVirtualMachinesWithAdvancedRole;
 
-    @ConfigProperty(name = "azure.global.max-vms.role.basic", defaultValue = "0")
+    @ConfigProperty(name = "max-vms.role.basic", defaultValue = "0")
     int maxBasicVirtualMachinesWithBasicRole;
 
     public int byRole(Role role) {
